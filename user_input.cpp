f4a8e75f20cc0bb28438e393559c841de092b090
GiftonNdemo <giftonndemoosindi@gmail.com>
2023-11-11 09:33:43 +0300

    working with user input

---
+++ user_input.cpp
@@ -0,0 +1,11 @@
+//C++ program that takes input from a user and prints it out for the user.
+#include<iostream>
+using namespace std;
+int main(){
+	int x;
+	cout<<"Enter a number; ";
+	cin>>x;
+	cout<<"The number is: " << x;
+	cout<<"\n";
+return 0;
+}
