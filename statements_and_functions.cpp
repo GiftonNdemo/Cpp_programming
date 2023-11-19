83abc0c2db9621b89c8ff3ead8d38a8fa461648c
GiftonNdemo <giftonndemoosindi@gmail.com>
2023-11-19 06:32:45 +0300

    updated with use of strings in c++

--- statements_and_functions.cpp
+++ statements_and_functions.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int add_numbers(int first, int second){
     int addition = first + second;
@@ -7,12 +8,15 @@ int add_numbers(int first, int second){
 }
 int main(){
     int first_number;
-    int second_number; 
+    int second_number;
+    string fullname;
+    std::cout<<"Enter your full name: ";
+    getline(cin,fullname);
     std::cout<<"Enter the first number: ";
     std::cin>>first_number;
     std::cout<<"Enter the second number: ";
     std::cin>>second_number;
     int sum = add_numbers(first_number, second_number);
-    std::cout<<"The sum of the two numbers is: "<<sum <<std::endl;
+    std::cout<<fullname<<", The sum of the two numbers is: "<<sum <<std::endl;
     return 0;
 }
