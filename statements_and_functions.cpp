dd971d824b978f85ef23fc59de68f31fc07cbc19
GiftonNdemo <giftonndemoosindi@gmail.com>
2023-11-20 12:42:57 +0300

    updated

--- statements_and_functions.cpp
+++ statements_and_functions.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
-#include<string> // use of the string library 
-using namespace std;
+#include<string> // use of the string library
 int add_numbers(int first, int second){
     int addition = first + second;
     return addition;
@@ -9,9 +8,9 @@ int add_numbers(int first, int second){
 int main(){
     int first_number;
     int second_number;
-    string fullname; // declaring a string variable in C++
+    std::string fullname; // declaring a string variable in C++
     std::cout<<"Enter your full name: ";
-    getline(cin,fullname); //getline function in string library - used to capture input of the user including whitespaces which the cin function doesn't capture.
+    getline(std::cin,fullname); //getline function in string library - used to capture input of the user including whitespaces which the cin function doesn't capture.
     std::cout<<"Enter the first number: ";
     std::cin>>first_number;
     std::cout<<"Enter the second number: ";
