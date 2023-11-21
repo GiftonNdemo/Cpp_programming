c699396501ad897d00682750b18e8482a254d253
GiftonNdemo <giftonndemoosindi@gmail.com>
2023-11-21 06:09:39 +0300

    conditional statements in C++

--- conditions.cpp
+++ conditions.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
+#include<string>
 int main(){
     //program to find the max number of two inputs
-    int a, b;
+    int a, b, time;
     std::cout<< "Enter the first number: ";
     std::cin >>a;
     std::cout<< "Enter the second number: ";
@@ -15,5 +16,16 @@ int main(){
     else{
         std::cout<<"The second number is greater than the first number!"<<std::endl;
     }
+//there is a shorthand form for writting if...else statements which is ;
+//variable = (condition) ? condition_true : condition_false;
+//we can then finally print out the result stored in the variable
+//i.e., std::cout<< variable;
+//
+//example program using shorthand if...else
+
+    std::cout<<"Enter the time in hrs:";
+    std::cin>>time;
+    std::string time_of_the_day = (time<1200) ? "It's Morning! Good Morning!": (time > 1200 && time <1501) ? "It's afternoon! Good afternoon" : (time >= 1501 && time < 1901) ? "It's evening! Good evening!": (time >= 1901 && time <= 2400) ? "It's night-time! Goodnight!":(time==1200)?"It's noon! Good Afternoon!" : "is not within the hrs time-frame!";
+    std::cout<<time_of_the_day<<std::endl;
 return 0;
 }
